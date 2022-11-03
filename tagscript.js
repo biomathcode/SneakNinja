0cfa2713e1975aae411f4de4241a24a708ea9d92
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-11-03 14:56:03 +0530

    added the tagscript

---
+++ tagscript.js
@@ -0,0 +1,9 @@
+// this will be the Js script that the users will add on there website.
+// it will make a call on the main_app rest api to track user data.
+// Collect data like country, time, id, session, device information.
+// Other features like cursor heatmanp can be added afterwards.
+
+(() => {
+  this.a = "Something";
+  return console.log("something");
+})();
